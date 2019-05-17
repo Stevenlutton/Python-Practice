9a7e79cd77a9193a42885760a53c72c6f7415726
Stevenlutton <steven.lutton2128@gmail.com>
2019-05-17 10:54:11 -0600

    edited tix

--- tickets.py
+++ tickets.py
@@ -1,1 +1,23 @@
+TICKET_PRICE = 10
+
+tickets_remaining = 100
+
+print("There are {} tickets remaining".format(tickets_remaining))
+
+name = input("Hi there, what is your name?   ")
+
+tickets_wanted = input("Hi {}, how many tickets would you like to purchase?  ".format(name))
+tickets_wanted = int(tickets_wanted)
+
+amt_due = tickets_wanted * TICKET_PRICE
+
+print("The total due is ${}".format(amt_due))
+
+should_proceed = input("Would you like to purchase?   Y/N  ")
+    if should_proceed.lower() == "y" :
+        print("SOLD!")
+tickets_remaining -= tickets_wanted
+
+else:
+    print("Thank you anyways, {}".format(name))
 
